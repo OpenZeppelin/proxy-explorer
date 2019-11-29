69d229ec68276dbd271a9722ebf758fd39713f4f
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-29 19:41:41 -0300

    Improve navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,8 +4,14 @@ import './index.css';
 export default function() {
   return (
     <ul className='NavBar'>
-      <li>Proxies</li>
-      <li>Github</li>
+      <li><NavBarLink url='' text='Proxies' /></li>
+      <li><NavBarLink url='https://github.com/OpenZeppelin/proxy-explorer' text='GitHub' /></li>
     </ul>
+  );
+}
+
+function NavBarLink({ url, text }) {
+  return (
+    <a target='_blank' rel='noopener noreferrer' href={url}>{text}</a>
   );
 }
